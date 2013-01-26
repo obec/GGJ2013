b2eb516372c61b8e2f40eab024a713133758d4f6
jesse <jesse.calkin@gmail.com>
2013-01-26 05:04:08 -0700

    updated Constants.java with some more audio refs

--- GGJ2013/src/com/teammeatstick/game/Constants.java
+++ GGJ2013/src/com/teammeatstick/game/Constants.java
@@ -4,5 +4,8 @@ public class Constants {
 	//Audio resources
 	public static final String VENOM_BOMB = "sounds/music/alien_venom_bomb.mp3";
 	public static final String HEART_BEAT = "sounds/effects/heart beat normal.mp3";
+	public static final String POWER_UP = "";
+	public static final String COLLIDE = "";
+	//what else? die? victory? menu selections?
 
 }
