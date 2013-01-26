90a5b52d027e2df2f4c7640bda9ddec2ef9f831f
Caleb Smith <calebthemeraglim@gmail.com>
2013-01-25 23:37:47 -0700

    Changed desktop resolution. Also adding test background to practice scrolling.

--- GGJ2013-desktop/src/com/teammeatstick/game/Main.java
+++ GGJ2013-desktop/src/com/teammeatstick/game/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "GGJ2013";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 1000;
+		cfg.height = 600;
 		
 		new LwjglApplication(new MainGame(), cfg);
 	}
