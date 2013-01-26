287ab3851a97b2f2751fc06b412fbafc6073fe4c
Robert Merritt <cynicalmelonthrope@gmail.com>
2013-01-26 13:21:20 -0700

    added sounds to constants file

--- GGJ2013/src/com/teammeatstick/game/Constants.java
+++ GGJ2013/src/com/teammeatstick/game/Constants.java
@@ -2,10 +2,19 @@ package com.teammeatstick.game;
 
 public class Constants {
 	//Audio resources
-	public static final String VENOM_BOMB = "sounds/music/alien_venom_bomb.mp3";
-	public static final String HEART_BEAT = "sounds/effects/heart beat normal.mp3";
+	public static final String HEART_BEAT_NORMAL = "sounds/effects/heart_beat_normal.mp3";
+	public static final String HEART_BEAT_SLOW = "sounds/effects/heart_beat_slow.mp3";
+	public static final String HEART_BEAT_FAST = "sounds/effects/heart_beat_fast.mp3";
 	public static final String POWER_UP = "";
-	public static final String COLLIDE = "";
+	public static final String COLLIDE_VIRUS = "sounds/effects/squish_virus_collide.mp3";
+	public static final String COLLIDE_NANO = "sounds/effects/Nanobot_crash.mp3";
+	public static final String BZZSHHHHT = "sounds/effects/bzsshhh.mp3";
+	public static final String CREDIT_MUSIC = "sounds/music/credits_music.mp3";
+	public static final String INGAME_MUSIC = "sounds/music/ingame_music.mp3";
+	public static final String MENU_MUSIC = "";
+	public static final String NANO_VICTORY = "";
+	public static final String VIRUS_VICTORY = "";
+	public static final String MENU_SELECTION = "";
 	//what else? die? victory? menu selections?
 
 }
