75938774e72237596e721c3e4c539287b8fd6faa
Chris Padgett <chris.e.padgett@gmail.com>
2013-01-26 05:51:12 -0700

    Added SpriteAnimator Class

--- GGJ2013/src/com/teammeatstick/game/MainGame.java
+++ GGJ2013/src/com/teammeatstick/game/MainGame.java
@@ -1,14 +1,27 @@
 package com.teammeatstick.game;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import sun.applet.Main;
+
+import SpriteSheet.BufferedImageLoader;
+import SpriteSheet.SpriteSheet;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g3d.Animator;
 
 public class MainGame implements ApplicationListener {
 	private OrthographicCamera camera;
@@ -16,23 +29,37 @@ public class MainGame implements ApplicationListener {
 	private Texture texture;
 	private Sprite sprite;
 	
+	private static final int        FRAME_COLS = 2;         // #1
+    private static final int        FRAME_ROWS = 2;         // #2
+    
+    Animation                       walkAnimation;          // #3
+    Texture                         walkSheet;              // #4
+    TextureRegion[]                 walkFrames;             // #5
+    SpriteBatch                     spriteBatch;            // #6
+    TextureRegion                   currentFrame;           // #7
+    
+    float stateTime;                                        // #8
+	private SpriteAnimator _spriteAnimator;
+    
 	@Override
 	public void create() {		
-		float w = Gdx.graphics.getWidth();
+		/*float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		
 		camera = new OrthographicCamera(1, h/w);
 		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
+		texture = new Texture(Gdx.files.internal("textures/sprites/BacteriaHero.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+		TextureRegion region = new TextureRegion(texture, 128, 128);
+		
 		
 		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
+		sprite.setSize(.1f,.1f);
 		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);*/
+		_spriteAnimator = new SpriteAnimator();
+		_spriteAnimator.create();
 	}
 
 	@Override
@@ -43,13 +70,14 @@ public class MainGame implements ApplicationListener {
 
 	@Override
 	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		/*Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		sprite.draw(batch);
-		batch.end();
+		batch.end();*/
+		_spriteAnimator.render();
 	}
 
 	@Override
